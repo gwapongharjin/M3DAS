0b41c7d19ea4725328b20cf51303c2b229999a72
Harjin Pabla <harjin.pabla@gmail.com>
2020-07-20 17:25:34 +0800

    Changed the color theme

--- app/src/main/java/com/m3das/biomech/uplb/HaulOthersFragment.java
+++ app/src/main/java/com/m3das/biomech/uplb/HaulOthersFragment.java
@@ -58,6 +58,6 @@ public class HaulOthersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_haul_others, container, false);
+        return inflater.inflate(R.layout.haul_others_fragment, container, false);
     }
 }
